9afa3e2912a886acd03f4f9e882eee1380476685
Administrator <19144720817@sina.cn>
2023-08-05 22:06:02 +0800

    Changes

--- src/test/java/javaStudyDemo/Subline.java
+++ src/test/java/javaStudyDemo/Subline.java
@@ -1,6 +1,11 @@
 package javaStudyDemo;
 
 public class Subline {
+    /**
+     * 1.数组的动态初始化
+     * 数组的定义 数据类型[] 数组名 = new 数据类型[大小]
+     */
+
     static double[] hens = {1.1,55.2,633.96,2.4};
 
     public static void main(String[] args) {
