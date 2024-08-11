977fa4bbca46f4c5f31da689da0b1e321d0e23ac
xinxin9508 <944714679@qq.com>
2024-08-11 14:49:36 +0800

    1

--- src/test/java/Base/Extends/Base.java
+++ src/test/java/Base/Extends/Base.java
@@ -6,9 +6,9 @@ public class Base {
     int n3 = 30;
     private int n4 = 40;
 
-//    public Base(){
-//        System.out.println("base构造器");
-//    }
+    public Base(){
+        System.out.println("base构造器");
+    }
 
     public Base(String name,int age){
         System.out.println("父类的有参构造器");
--- src/test/java/Base/Extends/ExtendsDetail.java
+++ src/test/java/Base/Extends/ExtendsDetail.java
@@ -5,6 +5,7 @@ package Base.Extends;
  * 2.默认调用父类的无参构造器，如果没有无参构造器，则要用super特指父类的构造器去完成初始化
  * 3.super()和this()必须放在子类方法的第一行，且两者不能同时出现
  * 4.不写super()，默认存在这个方法
+ * 5.super只能在构造器中使用，不能在成员方法中使用
  */
 public class ExtendsDetail {
     public static void main(String[] args) {
